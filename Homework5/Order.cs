d04dc936ee70707599383494cec940a84f6003e2
JDKpyy <1759447228@qq.com>
2020-04-09 21:38:43 +0800

    第5次作业

---
+++ Homework5/Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    class Order : IComparable
+    {
+        public int OrderNum { set; get; }
+        public string Customer { set; get; }
+        public List<OrderItem> orderItems;
+
+        public Order(int ordernumber, string customer, List<OrderItem> items)
+        {
+            OrderNum = ordernumber;
+            Customer = customer;
+            orderItems = items;
+        }
+
+        public int TotalPrice()
+        {
+            int total = 0;
+            foreach (OrderItem item in orderItems)
+                total += item.UnitPrice * item.Number;
+            return total;
+        }
+        public override string ToString()
+        {
+            string items = "";
+            foreach (OrderItem item in orderItems)
+                items += item;
+            return $"订单号:{OrderNum};\t{items};\t商品总价:{TotalPrice()};\t购买客户:{Customer}";
+        }
+
+        public int CompareTo(Object obj2)
+        {
+            if (!(obj2 is Order))
+                throw new System.ArgumentException();
+            Order order2 = obj2 as Order;
+            return this.OrderNum.CompareTo(order2.OrderNum);
+        }
+    }
+}
---
+++ Homework5/OrderService.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    class OrderService
+    {
+        public List<Order> orders = new List<Order>();
+        public void AddOrder(string cusname, List<OrderItem> items)
+        {
+            int count = 0;
+            foreach (Order order in orders)
+                count++;
+            orders.Add(new Order(count, cusname, items));
+        }
+
+        public Order SearchOrderNum(int num)
+        {
+            var order = orders.Where(or => or.OrderNum == num)
+                .OrderBy(or => or.TotalPrice());
+            Order[] ods = order.ToArray<Order>();
+            if (ods.Length == 0)
+                throw new OrderException("无此订单");
+            return ods[0];
+        }
+
+        public Order[] SearchItemName(string name)
+        {
+            var order = orders.Where(or => or.orderItems.Contains(new OrderItem(name, 0, 0)))
+                .OrderBy(or => or.TotalPrice());
+            Order[] ods = order.ToArray<Order>();
+            if (ods.Length == 0)
+                throw new OrderException("无此订单");
+            Foreach(ods, m => Console.WriteLine(m));
+            return ods;
+        }
+
+        public Order[] SearchCustomer(string cus)
+        {
+            var order = orders.Where(or => or.Customer == cus)
+                .OrderBy(or => or.TotalPrice());
+            Order[] ods = order.ToArray<Order>();
+            if (ods.Length == 0)
+                throw new OrderException("无此订单");
+            Foreach(ods, m => Console.WriteLine(m));
+            return ods;
+        }
+
+        public void DeleteOrder(int num)
+        {
+            Order order = SearchOrderNum(num);
+            orders.Remove(order);
+        }
+
+        public void SetOrderNum(int prenum, int num)
+        {
+            Order order = SearchOrderNum(prenum);
+            for (int i = 0; i < orders.Count; i++)
+                if (orders[i].OrderNum == num)
+                    throw new OrderException("订单号已存在");
+            order.OrderNum = num;
+        }
+
+        public void SetItemName(int num, int itemnum, string name)
+        {
+            Order order = SearchOrderNum(num);
+            order.orderItems[itemnum].Name = name;
+        }
+
+        public void SetItemNum(int num, int item, int itemnum)
+        {
+            Order order = SearchOrderNum(num);
+            order.orderItems[item].Number = itemnum;
+        }
+
+        public void SetItemPrice(int num, int item, int itemprice)
+        {
+            Order order = SearchOrderNum(num);
+            order.orderItems[item].UnitPrice = itemprice;
+        }
+
+        public void SetCusName(int num, string name)
+        {
+            Order order = SearchOrderNum(num);
+            order.Customer = name;
+        }
+
+        public void OrderSort()
+        {
+            orders.Sort();
+        }
+
+        public void Delete(int ordermnum)
+        {
+            if (orders[ordermnum] == null)
+                throw new OrderException("无此订单");
+            orders.Remove(orders[ordermnum]);
+        }
+
+        private static void Foreach(Order[] orderlist, Action<Order> action)
+        {
+            for (int i = 0; i < orderlist.Length; i++)
+                action(orderlist[i]);
+        }
+    }
+}
