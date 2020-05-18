9b2c799efd22e00310d6c21c59c5aeeecf921ed5
JDKpyy <1759447228@qq.com>
2020-05-18 17:41:31 +0800

    第77次作业

---
+++ Homework7/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Homework7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.deep = new System.Windows.Forms.TextBox();
+            this.M_leng = new System.Windows.Forms.TextBox();
+            this.R_per1 = new System.Windows.Forms.TextBox();
+            this.L_per2 = new System.Windows.Forms.TextBox();
+            this.R_th1 = new System.Windows.Forms.TextBox();
+            this.L_th2 = new System.Windows.Forms.TextBox();
+            this.color = new System.Windows.Forms.TextBox();
+            this.Begin_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(614, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "递归深度";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(614, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "主干长度";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(584, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "右分支长度比";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(584, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "左分支长度比";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(599, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "右分支角度";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(599, 278);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "左分支角度";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(511, 323);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(171, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "画笔颜色(such as red)";
+            // 
+            // deep
+            // 
+            this.deep.Location = new System.Drawing.Point(688, 18);
+            this.deep.Name = "deep";
+            this.deep.Size = new System.Drawing.Size(100, 25);
+            this.deep.TabIndex = 7;
+            // 
+            // M_leng
+            // 
+            this.M_leng.Location = new System.Drawing.Point(688, 68);
+            this.M_leng.Name = "M_leng";
+            this.M_leng.Size = new System.Drawing.Size(100, 25);
+            this.M_leng.TabIndex = 8;
+            // 
+            // R_per1
+            // 
+            this.R_per1.Location = new System.Drawing.Point(688, 111);
+            this.R_per1.Name = "R_per1";
+            this.R_per1.Size = new System.Drawing.Size(100, 25);
+            this.R_per1.TabIndex = 9;
+            // 
+            // L_per2
+            // 
+            this.L_per2.Location = new System.Drawing.Point(688, 163);
+            this.L_per2.Name = "L_per2";
+            this.L_per2.Size = new System.Drawing.Size(100, 25);
+            this.L_per2.TabIndex = 10;
+            // 
+            // R_th1
+            // 
+            this.R_th1.Location = new System.Drawing.Point(688, 216);
+            this.R_th1.Name = "R_th1";
+            this.R_th1.Size = new System.Drawing.Size(100, 25);
+            this.R_th1.TabIndex = 11;
+            // 
+            // L_th2
+            // 
+            this.L_th2.Location = new System.Drawing.Point(688, 267);
+            this.L_th2.Name = "L_th2";
+            this.L_th2.Size = new System.Drawing.Size(100, 25);
+            this.L_th2.TabIndex = 12;
+            // 
+            // color
+            // 
+            this.color.Location = new System.Drawing.Point(688, 313);
+            this.color.Name = "color";
+            this.color.Size = new System.Drawing.Size(100, 25);
+            this.color.TabIndex = 13;
+            // 
+            // Begin_btn
+            // 
+            this.Begin_btn.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Begin_btn.Location = new System.Drawing.Point(605, 383);
+            this.Begin_btn.Name = "Begin_btn";
+            this.Begin_btn.Size = new System.Drawing.Size(183, 55);
+            this.Begin_btn.TabIndex = 14;
+            this.Begin_btn.Text = "绘制";
+            this.Begin_btn.UseVisualStyleBackColor = true;
+            this.Begin_btn.Click += new System.EventHandler(this.Begin_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Begin_btn);
+            this.Controls.Add(this.color);
+            this.Controls.Add(this.L_th2);
+            this.Controls.Add(this.R_th1);
+            this.Controls.Add(this.L_per2);
+            this.Controls.Add(this.R_per1);
+            this.Controls.Add(this.M_leng);
+            this.Controls.Add(this.deep);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox deep;
+        private System.Windows.Forms.TextBox M_leng;
+        private System.Windows.Forms.TextBox R_per1;
+        private System.Windows.Forms.TextBox L_per2;
+        private System.Windows.Forms.TextBox R_th1;
+        private System.Windows.Forms.TextBox L_th2;
+        private System.Windows.Forms.TextBox color;
+        private System.Windows.Forms.Button Begin_btn;
+    }
+}
+
---
+++ Homework7/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Begin_btn_Click(object sender, EventArgs e)
+        {
+            if (graphics == null) graphics = this.CreateGraphics();
+            try
+            {
+                n = Int32.Parse(deep.Text);
+                leng = Int32.Parse(M_leng.Text);
+                per1 = Double.Parse(R_per1.Text);
+                per2 = Double.Parse(L_per2.Text);
+                th1 = (Int32.Parse(R_th1.Text)) * Math.PI / 180;
+                th1 = (Int32.Parse(L_th2.Text)) * Math.PI / 180;
+            }
+            catch
+            {
+            }
+            drawCayleyTree(n, 200, 310, leng, -Math.PI / 2);
+        }
+
+        private Graphics graphics;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+        int n = 0;
+        int leng = 0;
+
+        void drawCayleyTree(int n,double x0,double y0,double leng,double th)
+        {
+            if (n == 0) return;
+
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1);
+
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+        }
+
+        void drawLine(double x0,double y0,double x1, double y1)
+        {
+            switch (color.Text)
+            {
+                case "red":
+                    graphics.DrawLine(Pens.Red, (int)x0, (int)y0, (int)x1, (int)y1);
+                    break;
+                case "blue":
+                    graphics.DrawLine(Pens.Blue, (int)x0, (int)y0, (int)x1, (int)y1);
+                    break;
+                case "yellow":
+                    graphics.DrawLine(Pens.Yellow, (int)x0, (int)y0, (int)x1, (int)y1);
+                    break;
+                case "green":
+                    graphics.DrawLine(Pens.Green, (int)x0, (int)y0, (int)x1, (int)y1);
+                    break;
+                default:
+                    graphics.DrawLine(Pens.Black, (int)x0, (int)y0, (int)x1, (int)y1);
+                    break;
+            }
+        }
+    }
+}
